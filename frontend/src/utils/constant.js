392e191e7884f319909d888a2c73615d6e6c1e75
Shubh Doshi <shubhdoshi21@gmail.com>
2025-02-19 00:12:40 +0530

    extract done

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,6 +4,6 @@ export const APPLICATION_API_END_POINT =
   "http://localhost:8000/api/v1/application";
 export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
 export const RESUME_API_END_POINT = "http://localhost:8000/api/v1/resume";
-// export const PYTHON_API_END_POINT =
-//   "https://python-job-portal-backend.onrender.com";
-export const PYTHON_API_END_POINT = "http://127.0.0.1:5000";
+export const PYTHON_API_END_POINT =
+  "https://python-job-portal-backend.onrender.com";
+// export const PYTHON_API_END_POINT = "http://127.0.0.1:5000";
